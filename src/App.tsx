8e27edc50677e1d7b5b9d4c19862ef80ae7a4161
Nataly Araujo <nataly.araujo@brainweb.com.br>
2020-11-25 07:41:31 +0100

    Top app bar implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import HomePage from './pages/homepage'
 import InformationPage from './pages/information'
@@ -12,19 +11,6 @@ function App() {
   return (
     <Router>
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/information">Information Page</Link>
-          </li>
-          <li>
-            <Link to="/services">Services Page</Link>
-          </li>
-        </ul>
-      </nav>
       <Switch>
           <Route path="/information">
             <InformationPage />
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 const HomePage = () => (
-  <div className='homepage'>
+ 
     <Container maxWidth="sm">
       <Box my={8}>
         <Typography variant="h4" component="h1" gutterBottom>
@@ -12,7 +12,7 @@ const HomePage = () => (
         </Typography>
       </Box>
     </Container>
-  </div>
+ 
 );
 
 export default HomePage;
